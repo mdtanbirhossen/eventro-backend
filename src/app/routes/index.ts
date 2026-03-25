4b1e90dba731c45f5d72d4d3c6abefbd17066441
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2026-03-25 19:34:20 +0600

    feat: added upload route

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { AdminRoutes } from "../module/admin/admin.route";
 import { AuthRoutes } from "../module/auth/auth.route";
 import { UserRoutes } from "../module/user/user.route";
+import { UploadRoutes } from "../module/upload/upload.route";
 
 const router = Router();
 
 router.use("/auth", AuthRoutes);
 router.use("/users", UserRoutes);
 router.use("/admins", AdminRoutes);
+router.use("/upload", UploadRoutes);
 
 export const IndexRoutes = router;
