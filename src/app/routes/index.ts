82ad3c1834d1d3d8322a7c1a5967f8981245e088
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2026-03-26 18:39:59 +0600

    fix: updated notification types

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,6 +3,10 @@ import { AdminRoutes } from "../module/admin/admin.route";
 import { AuthRoutes } from "../module/auth/auth.route";
 import { UserRoutes } from "../module/user/user.route";
 import { UploadRoutes } from "../module/upload/upload.route";
+import { EventRoutes } from "../module/event/event.route";
+import { EventCategoryRoutes } from "../module/eventCategory/eventCategory.route";
+import { ReviewRoutes } from "../module/review/review.route";
+import { NotificationRoutes } from "../module/notification/notification.route";
 
 const router = Router();
 
@@ -10,5 +14,9 @@ router.use("/auth", AuthRoutes);
 router.use("/users", UserRoutes);
 router.use("/admins", AdminRoutes);
 router.use("/upload", UploadRoutes);
+router.use("/events", EventRoutes);
+router.use("/event-categories", EventCategoryRoutes);
+router.use("/reviews", ReviewRoutes);
+router.use("/notifications", NotificationRoutes);
 
 export const IndexRoutes = router;
